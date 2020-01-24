b37a7a12db1ec0a64675a463b025c504ca3a83cd
erocknine <echeungsk@gmail.com>
2020-01-24 17:35:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -36,7 +36,7 @@ def total_gross(source)
   over_total = 0
   index = 0
   while index < source.length do
-    over_total += source[index][:name]
+    over_total += 
     index+=1
   end
   over_total
